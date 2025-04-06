4d8f32e3783746cffa25fd67e80e8104dc2575b0
Darrell <zh0521@gmail.com>
2025-04-06 16:03:30 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,8 +19,8 @@
 		<About />
 		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
 			<Meetups />
-			<Mint />
-			<!-- <Node />
+			<!-- <Mint />
+			<Node />
 			<Apps /> -->
 		</div>
 		<!-- <Onboarding /> -->
