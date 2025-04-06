fb847c22b4a555486f067f9fa1c462a3ef04fcc0
Darrell <zh0521@gmail.com>
2025-04-06 15:28:00 +0900

    add footer changes

--- src/components/FooterButton.svelte
+++ src/components/FooterButton.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
-    export let url: string;
-    export let icon: string;
-    export let w: string;
-    export let h: string;
-    export let p: string;
+  export let svg: string | undefined;
+  export let icon: string | undefined;
+  export let url: string;
+  export let w: string;
+  export let h: string;
+  export let p: string;
 </script>
 
 <a href={url} class="inline-flex items-center align-middle">
     <button
         class={`flex gap-2 items-center w-16 h-16 dark:text-gray-50 text-gray-700 cursor-pointer`}
     >
-        <svg
-            fill="#ffffff"
-            class="w-5 h-5 fill-current"
-            viewBox={`0 0 ${w} ${h}`}
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path d={icon} />
-        </svg>
+        {#if svg}
+            <!-- Render the SVG file as an <img> tag -->
+            <img src={svg} alt="Footer Icon" class={`w-${w} h-${h} p-${p}`} />
+        {:else if icon}
+            <!-- Render the icon as an <svg> element -->
+            <svg
+                fill="#ffffff"
+                class="w-5 h-5 fill-current"
+                viewBox={`0 0 ${w} ${h}`}
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path d={icon} />
+            </svg>
+        {/if}
     </button>
 </a>
