dfb480895e41e7c832404dcb156a4d703a912751
Darrell <zh0521@gmail.com>
2025-04-07 11:54:25 +0900

    fix i18n not ready

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-	import "../app.css";
-	import Header from "../components/Header.svelte";
-	import Footer from "../components/Footer.svelte";
-	import { setupI18n } from "../services/i18n";
-	import { Toaster } from "svelte-french-toast";
+    import "../app.css";
+    import Header from "../components/Header.svelte";
+    import Footer from "../components/Footer.svelte";
+    import { setupI18n } from "../services/i18n";
+    import { Toaster } from "svelte-french-toast";
+    import { onMount } from "svelte";
 
-	setupI18n({ withLocale: "en" });
+    let i18nReady = false;
+
+    // Ensure setupI18n completes before rendering
+    onMount(async () => {
+        await setupI18n({ withLocale: "en" });
+        i18nReady = true;
+    });
 </script>
 
-<div
-	class="container relative mx-auto flex min-h-screen w-full max-w-[1800px] flex-col text-sm sm:text-base"
->
-	<Toaster />
-	<Header />
-	<slot />
-	<Footer />
-</div>
+{#if i18nReady}
+    <div
+        class="container relative mx-auto flex min-h-screen w-full max-w-[1800px] flex-col text-sm sm:text-base"
+    >
+        <Toaster />
+        <Header />
+        <slot />
+        <Footer />
+    </div>
+{/if}
