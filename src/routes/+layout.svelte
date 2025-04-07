90a9977f33c9e9cbf2bf2d84defa09eb14038627
Darrell <zh0521@gmail.com>
2025-04-07 12:22:36 +0900

    Revert "fix i18n not ready"
    This reverts commit dfb480895e41e7c832404dcb156a4d703a912751.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,18 @@
 <script lang="ts">
-    import "../app.css";
-    import Header from "../components/Header.svelte";
-    import Footer from "../components/Footer.svelte";
-    import { setupI18n } from "../services/i18n";
-    import { Toaster } from "svelte-french-toast";
-    import { onMount } from "svelte";
+	import "../app.css";
+	import Header from "../components/Header.svelte";
+	import Footer from "../components/Footer.svelte";
+	import { setupI18n } from "../services/i18n";
+	import { Toaster } from "svelte-french-toast";
 
-    let i18nReady = false;
-
-    // Ensure setupI18n completes before rendering
-    onMount(async () => {
-        await setupI18n({ withLocale: "en" });
-        i18nReady = true;
-    });
+	setupI18n({ withLocale: "en" });
 </script>
 
-{#if i18nReady}
-    <div
-        class="container relative mx-auto flex min-h-screen w-full max-w-[1800px] flex-col text-sm sm:text-base"
-    >
-        <Toaster />
-        <Header />
-        <slot />
-        <Footer />
-    </div>
-{/if}
+<div
+	class="container relative mx-auto flex min-h-screen w-full max-w-[1800px] flex-col text-sm sm:text-base"
+>
+	<Toaster />
+	<Header />
+	<slot />
+	<Footer />
+</div>
--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,26 +1,14 @@
-import { dictionary, locale, _, waitLocale, init } from "svelte-i18n";
+import { dictionary, locale, _ } from "svelte-i18n";
 import en from "./locales/en/common.json";
 import es from "./locales/es/common.json";
 import eu from "./locales/eu/common.json";
-
-async function setupI18n({ withLocale: _locale } = { withLocale: "en" }) {
-  // Initialize svelte-i18n with a fallback locale
-  init({
-    fallbackLocale: "en", // Set your fallback locale here
-  });
-
-  // Set the dictionary with available translations
+function setupI18n({ withLocale: _locale } = { withLocale: "en" }) {
   dictionary.set({
     en: en,
     es: es,
     eu: eu,
   });
-
-  // Set the initial locale
   locale.set(_locale);
-
-  // Wait for the locale to be ready
-  await waitLocale();
 }
 
 export { _, setupI18n };
